adb2e00297421b1cdbb649e1debc821b7319cc08
Art-Vis <artyom0698@bk.ru>
2023-11-08 16:00:15 +0300

    two commit

---
+++ js/api.js
@@ -0,0 +1,14 @@
+const API_URL = 'https://likeable-inexpensive-cotton.glitch.me/api';
+
+export const getData = async (url) => {
+  try {
+    const response = await fetch(`${API_URL}${url}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка при получении данных:", error);
+    throw error;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-import { convertStringNumber } from "./convertStringNumber.js";
+import { getData } from "./api.js";
+import { convertStringNumber, reformatDate } from "./helper.js";
+
+const typesOperation = {
+  income: 'Доход',
+  expenses: 'Расход',
+};
 
 const financeForm = document.querySelector('.finance__form');
 const financeAmount = document.querySelector('.finance__amount');
 const financeReport = document.querySelector('.finance__report');
+const reportOperationList = document.querySelector('.report__operation-list');
 
 const report = document.querySelector('.report');
-const reportClose = document.querySelector('.report__close');
+const reportDates = document.querySelector('.report__dates');
+
 
 let amount = 0;
 financeAmount.textContent = amount;
@@ -28,14 +36,69 @@ financeForm.addEventListener('submit', (e) => {
   financeAmount.textContent = `${amount.toLocaleString()} ₽`;
 });
 
-financeReport.addEventListener('click', () => {
-  if (!report.classList.contains('report__open')) {
-    report.classList.add('report__open');
+const closeReport = ({ target }) => {
+  if (target.closest('.report__close') || !target.closest('.report') && target !== financeReport) {
+    report.classList.remove('report__open');
+    document.removeEventListener('click', closeReport);
   }
+};
+
+const openReport = () => {
+  report.classList.add('report__open');
+  document.addEventListener('click', closeReport);
+};
+
+const renderReport = (data) => {
+  reportOperationList.textContent = '';
+
+  const reportRows = data.map(({category,amount,description,date,type}) => {
+    const reportRow = document.createElement('tr');
+    reportRow.classList.add('report__row');
+    reportRow.innerHTML = `
+      <td class="report__cell">${category}</td>
+      <td class="report__cell" style="text-align:end">${amount.toLocaleString()}&nbsp;₽</td>
+      <td class="report__cell">${description}</td>
+      <td class="report__cell">${reformatDate(date)}</td>
+      <td class="report__cell">${typesOperation[type]}</td>
+      <td class="report__action-cell">
+        <button
+          class="report__button report__button_table">&#10006;</button>
+      </td>
+    `;
+    
+    return reportRow;
+  });
+
+  reportOperationList.append(...reportRows);
+};
+
+financeReport.addEventListener('click', async () => {
+  openReport();
+
+  const data = await getData('/test');
+  renderReport(data);
 });
 
-reportClose.addEventListener('click', () => {
-  if (report.classList.contains('report__open')) {
-    report.classList.remove('report__open');
+
+reportDates.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(reportDates));
+  console.log('formData: ', formData);
+
+  const searchParams = new URLSearchParams();
+
+  if (formData.startDate) {
+    searchParams.append('startDate', formData.startDate);
   }
+
+  if (formData.endtDate) {
+    searchParams.append('endtDate', formData.endDate);
+  }
+
+  const queryString = searchParams.toString();
+  const url = queryString ? `/test?${queryString}` : '/test';
+
+  const data = await getData(url);
+  renderReport(data);
 });
